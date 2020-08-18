9c4072291d982bb965e06334375cf2a8290c8040
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-08-18 22:16:54 +0300

    Create solution.java

---
+++ Pow(x, n)/solution.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+
+class Solution {
+    public static double myPow(double x, int n) {
+        if (n == 0) return 1;
+
+        double temp = myPow(x, n / 2);
+
+        if (n % 2 == 0)
+            return temp * temp;
+        else {
+            if (n > 0)
+                return x * temp * temp;
+            else
+                return (temp * temp) / x;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2.10000, 3));
+    }
+}
