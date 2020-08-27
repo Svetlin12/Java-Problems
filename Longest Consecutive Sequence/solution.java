69866d93348e0baa344e7be1a4e0855133e236f8
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-08-27 18:47:49 +0300

    Update solution.java

--- Longest Consecutive Sequence/solution.java
+++ Longest Consecutive Sequence/solution.java
@@ -8,12 +8,24 @@ public int longestConsecutive(int[] nums) {
             if (lengths.containsKey(num))
                 continue;
 
+            // get left number's stored length 
             int leftLen = lengths.getOrDefault(num - 1, 0);
+                
+            // get right number's stored length
             int rightLen = lengths.getOrDefault(num + 1, 0);
+                
+            // compute actual length (combine left and right length and add 1 for the current number)
             int len = leftLen + rightLen + 1;
+                
+            // update the hash map's values
             lengths.put(num, len);
+               
+            // update left end of interval
             lengths.put(num - leftLen, len);
+                
+            // update right end of interval
             lengths.put(num + rightLen, len);
+                
             max = Math.max(len, max);
         }
 
