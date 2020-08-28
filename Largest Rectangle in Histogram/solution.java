a1d71f1ff3d08b99292ced04cecf46fe0f3b9881
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-08-28 18:00:04 +0300

    Update solution.java

--- Largest Rectangle in Histogram/solution.java
+++ Largest Rectangle in Histogram/solution.java
@@ -4,17 +4,25 @@ import java.util.*;
 
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        // the stack will hold start indices and heights of the rectangles as we go through the array
         Stack<Pair<Integer, Integer>> s = new Stack<>();
         int cnt = 0, maxArea = 0, currArea = 0;
+        
         for (; cnt < heights.length; cnt++) {
             if (s.isEmpty()) {
                 Pair<Integer, Integer> p = new Pair<>(cnt, heights[cnt]);
                 s.add(p);
             }
             else {
+                // if the current height is smaller than the top of the stack, then get each index we pop so that we can
+                // extend the current rectangle in the left direction
                 int lastIndex = cnt;
+                
                 while (!s.isEmpty() && s.peek().getValue() > heights[cnt]) {
+                    // get starting index of what we are about to pop
                     lastIndex = s.peek().getKey();
+                    
+                    // compute the area of the rectangle that we got on top of the stack
                     currArea = (cnt - lastIndex) * s.peek().getValue();
                     if (maxArea < currArea)
                         maxArea = currArea;
@@ -26,6 +34,7 @@ class Solution {
             }
         }
 
+        // after the traversal there might be rectangles left in the stack which extend to the end of the array
         while (!s.empty()) {
             currArea = (cnt - s.peek().getKey()) * s.peek().getValue();
             if (maxArea < currArea)
